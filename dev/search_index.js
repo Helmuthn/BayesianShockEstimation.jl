1609501c3475dd09f1a0c40f28dc47479ea74c9d
Documenter.jl <documenter@juliadocs.github.io>
2022-10-14 14:31:54 +0000

    build based on a3452a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BayesianShockEstimation","category":"page"},{"location":"#BayesianShockEstimation","page":"Home","title":"BayesianShockEstimation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BayesianShockEstimation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BayesianShockEstimation]","category":"page"}]
+}
