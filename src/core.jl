b68c9eb88722e6337b49630afd59f30f034e43b4
Helmuth Naumer <hnaumer2@illinois.edu>
2022-10-23 13:12:20 -0500

    Added RTS Sampler

--- src/core.jl
+++ src/core.jl
@@ -27,9 +27,17 @@ export ShockParams
     BoundaryParams
 
 Struct containing all needed information for boundary sampling
+
+# Parameters
+ - `μ` : Mean at sample times
+ - `σ` : Standard deviation at sample times
+ - `t` : Sample times
+
 """
 struct BoundaryParams
-
+    μ::Array{Float64}
+    σ::Array{Float64}
+    t::Array{Float64}
 end
 export BoundaryParams
 
@@ -95,23 +103,111 @@ A list of approximate densities
 Uses the method of characteristics to find shocks.
 """
 function IncrementShockCounts!(shockcounts, systemparams::ShockParams, boundary)
+    # Find all pairwise intersection times
+    intersect_locs = zeros(100,100)
+    remaining_curves = 1:100
+
+    shocks = []
+    # Find closest interesctions, increment balls, then remove curves from list
     
+
+
 end
 export IncrementShockCounts!
 
 """
-    StepCharacteristics
-"""
-function StepCharacteristics()
+    ComputeIntersection(t1, u1, t2, u2)
 
+Helper function to compute the intersection between
+two characteristic curves in Burgers equation
+starting at x=0.
+"""
+function ComputeIntersection(t1, u1, t2, u2)
+    if u1 == u2
+        return nothing 
+    elseif (t2-t1)/(u2-u1) * u1 * u2 < 0
+        return nothing
+    else
+        return ((u2*t2 - u1*t2)/(u2-u1), (t2-t1)/(u2-u1) * u1 * u2)
+    end
 end
 
 """
     SampleBoundary(boundaryparams)
 
 Generate a sample of a boundary.
+
+# Arguments
+  - `boundaryparams`: Parameters characterizing the distribution of the boundary
+
+# Returns
+A function representing a sample of boundary conditions.
 """
 function SampleBoundary(boundaryparams::BoundaryParams)
-    
+    sample_count = length(boundaryparams.t)
+    σ = boundaryparams.σ
+    μ = boundaryparams.μ
+    return σ .* randn(sample_count) + μ
 end
 export SampleBoundary
+
+function RTS_sample(estimates, variances, RTSvariances, σ_w)
+    # Start by generating the noise
+    out = sqrt.(RTSvariances) .* randn(length(estimates))
+    
+    # Add in the means
+    for i in length(out)-1:-1:1
+        weight = σ_w^2/(σ_w^2 + variances[i])
+        out[i] += weight * estimates[i] + (1 - weight) * out[i+1]
+    end
+
+    return out
+end
+
+"""
+Helper Function computing variance for RTS sampler
+
+Returns forward estimates with RTS variances
+"""
+function RTS_Smooth(observations, σ_w, σ_v)
+    estimates, variances = KalmanFilterWalk(observations, σ_w, σ_v)
+    RTSvariances = BackwardsCovarianceWalk(variances, σ_w)
+    return estimates, variances, RTSvariances
+end
+export RTS_Smooth
+
+"""
+Helper function computing Kalman filter for the given model
+"""
+function KalmanFilterWalk(observations, σ_w, σ_v)
+    estimates = zeros(length(observations))
+    variances = zeros(length(observations))
+
+    # Initialize the Kalman filter
+    estimates[1] = observations[0]
+    variances[1] = σ_v^2
+
+    for i in 2:length(observations)
+        weight = σ_v^2/(variances[i-1] + σ_v^2 + σ_w^2)
+        estimates[i] = weight * estimates[i-1] + (1 - weight) * observations[i]
+        variances[i] = weight * (variances[i-1] + σ_w^2)
+    end
+
+    return estimates, variances
+end
+
+function BackwardsCovarianceWalk(variances, σ_w)
+    RTSvariances = zeros(length(variances))
+    for i in length(variances)-1:-1:1
+        weight = σ_w^2/(variances[i] + σ_w^2)
+        RTSvariances[i] = weight * variances[i] + (1-weight)^2 * RTSvariances[i+1]
+    end
+    return RTSvariances
+end
+
+"""
+Helper Function computing backwards smoothing according to RTS
+"""
+function BackwardsSample(estimates, variances, σ_w)
+    out = zeros()
+end
